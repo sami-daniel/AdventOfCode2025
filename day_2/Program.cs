6fa7c269d381ac59828e0e039bfd8e250e8bf280
Sami Daniel <samidanielpersonal@gmail.com>
2025-12-02 17:15:11 -0300

    Add initial project files including .gitignore, solution, and main program

---
+++ day_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = args[0];
+            var input = File.ReadAllLines(path).First();
+            Process(input);
+        }
+
+        static void Process(string input)
+        {
+            // I guess there is some way to do this using regex
+            var splited = input.Split(',');
+            long total = 0;
+            
+            foreach (var section in splited)
+            {
+                var values = section.Split('-');
+                var first = long.Parse(values[0]);
+                var second = long.Parse(values[1]);
+
+                for (long i = first; i <= second; i++)
+                {
+                    total += ShouldSum(i) ? i : 0; 
+                }
+            }
+
+            Console.WriteLine($"Output: {total}");
+        }        
+
+        static bool ShouldSum(long n, int endAt = 1)
+        {
+            var s = n.ToString();
+            var pattern = s[..endAt];
+            Console.WriteLine(s);
+            Console.WriteLine(pattern);
+            if (s.Length % pattern.Length == 0)
+            {
+                List<string> parts = [];
+                var length = s.Length / pattern.Length;
+
+                for (int i = 0; i < n; i++)
+                {
+                    parts.Add(s.Substring(i * length, length));
+                }
+
+                Console.WriteLine(parts);
+            }
+
+            return false;
+        }
+    }
+}
