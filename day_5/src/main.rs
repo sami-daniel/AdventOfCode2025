84409616264f6496fc0749943f51fc7449086eb3
Sami Daniel <samidanielpersonal@gmail.com>
2025-12-05 22:49:24 -0300

    Refactor range processing logic for improved clarity and efficiency

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,34 +1,57 @@
-use std::{collections::HashSet, env::args, fs::File, i32, io::Read, ops::{RangeBounds, RangeInclusive}};
+use std::{env::args, fs::File, io::Read, ops::RangeInclusive};
 
 fn main() {
     let args: Vec<String> = args().collect();
     let mut file_buffer = String::new();
 
     File::open(args.get(1).unwrap()).unwrap().read_to_string(&mut file_buffer).unwrap();
-    let total_lines: Vec<&str> = file_buffer[..].split('\n').collect();
-    let div_idx = total_lines.iter().position(|l| *l == "").expect("No separation line available");
-    let (ranges_str, _) = total_lines.split_at(div_idx);
+    let total_lines: Vec<&str> = file_buffer[..].split('\n').filter(|l| !l.is_empty()).collect();
     let mut ranges = vec![];
 
-    for range_str in ranges_str {
+    for range_str in total_lines {
         add_part(&range_str, &mut ranges);
     }
-
-    println!("Total: {}", total)
+    
+    let total: usize = ranges.iter().map(|r| r.end() - r.start() + 1).sum();
+    println!("Total: {}", total);
 }
 
 fn add_part(str: &str, ranges: &mut Vec<RangeInclusive<usize>>) {
     let range_parts = str.split('-').map(|p| p.parse::<usize>().unwrap()).take(2).collect::<Vec<usize>>();
-    let new_range = range_parts[0]..=range_parts[1];
+    let new_start = range_parts[0];
+    let new_end = range_parts[1];
 
-    for range in ranges.clone() {
-        if new_range.start() < range.start() {
-            ranges.push(*new_range.start()..=*range.start());
-        }
-
-        if new_range.end() < range.end() {
-            ranges.push(*new_range.end()..=*range.end());
-        }
+    if ranges.is_empty() {
+        ranges.push(new_start..=new_end);
+        return;
     }
 
+    let mut to_add = vec![(new_start, new_end)];
+    
+    for existing in ranges.iter() {
+        let mut next_to_add = vec![];
+        
+        for (start, end) in to_add {
+            let ex_start = *existing.start();
+            let ex_end = *existing.end();
+
+            if end < ex_start || start > ex_end {
+                next_to_add.push((start, end));
+            } else {
+                if start < ex_start {
+                    next_to_add.push((start, ex_start - 1));
+                }
+                
+                if end > ex_end {
+                    next_to_add.push((ex_end + 1, end));
+                }
+            }
+        }
+        
+        to_add = next_to_add;
+    }
+    
+    for (start, end) in &to_add {
+        ranges.push(*start..=*end);
+    }
 }
